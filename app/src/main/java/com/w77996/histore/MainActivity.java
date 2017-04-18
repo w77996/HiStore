9591977776cc581fd4d1728d9863268a887a6ebd
w77996 <912513404@qq.com>
2017-04-18 21:19:34 +0800

    toolbar样式添加

--- app/src/main/java/com/w77996/histore/MainActivity.java
+++ app/src/main/java/com/w77996/histore/MainActivity.java
@@ -3,7 +3,9 @@ package com.w77996.histore;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import butterknife.BindView;
@@ -15,14 +17,26 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawlayout;
     @BindView(R.id.main_nav_view)
     NavigationView mNavigationView;
+    @BindView(R.id.mian_toolbar)
+    Toolbar mToolbar;
 
     private View mHeaderView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
 
+    }
+
+    private void initView() {
+        // setSupportActionBar(mToolbar);
         mHeaderView = mNavigationView.getHeaderView(0);
+        mToolbar.inflateMenu(R.menu.toolbar_menu);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawlayout,mToolbar,R.string.open,R.string.close);
+        actionBarDrawerToggle.syncState();
+        mDrawlayout.addDrawerListener(actionBarDrawerToggle);
     }
 }
